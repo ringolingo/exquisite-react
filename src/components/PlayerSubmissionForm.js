9ab81b8bb4fd19beacfa21e3f071f5391340e405
Ringo Alcock <ringo.alcock@gmail.com>
2021-01-05 15:13:19 -0800

    add conditional classNames for invalid fields

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -116,22 +116,50 @@ const PlayerSubmissionForm = ({sendSubmission, index}) => {
         <div className="PlayerSubmissionForm__poem-inputs">
           {/* update these so they aren't just hardcoded to the formFields array indices */}
           The 
-          <input name='adj1' placeholder={ formFields[1]['placeholder'] } type="text" value={ formFields[1]['value'] } onChange={onInputChange} />
-          <input name='noun1' placeholder={ formFields[2]['placeholder'] } type="text" value={ formFields[2]['value'] } onChange={onInputChange} />
-          <input name='adv' placeholder={ formFields[3]['placeholder'] } type="text" value={ formFields[3]['value'] } onChange={onInputChange} />
-          <input name='verb' placeholder={ formFields[4]['placeholder'] } type="text" value={ formFields[4]['value'] } onChange={onInputChange} />
-          the
-          <input name='adj2' placeholder={ formFields[6]['placeholder'] } type="text" value={ formFields[6]['value'] } onChange={onInputChange} />
-          <input name='noun2' placeholder={ formFields[7]['placeholder'] } type="text" value={ formFields[7]['value'] } onChange={onInputChange} />
-          .
-          {/* {
-            // Put your form inputs here... We've put in one below as an example
-            // The adjective noun adverb verb the adjective noun
-          }
           <input
-            placeholder="hm..."
-            type="text" /> */}
-
+            name='adj1'
+            placeholder={ formFields[1]['placeholder'] }
+            type="text"
+            value={ formFields[1]['value'] }
+            className={ formFields[1]['value'] === '' ? 'PlayerSubmissionFormt__input--invalid' : 'PlayerSubmissionFormt__input--valid' }
+            onChange={onInputChange} />
+          <input
+            name='noun1'
+            placeholder={ formFields[2]['placeholder'] }
+            type="text"
+            value={ formFields[2]['value'] }
+            className={ formFields[2]['value'] === '' ? 'PlayerSubmissionFormt__input--invalid' : 'PlayerSubmissionFormt__input--valid' }
+            onChange={onInputChange} />
+          <input
+            name='adv'
+            placeholder={ formFields[3]['placeholder'] }
+            type="text"
+            value={ formFields[3]['value'] }
+            className={ formFields[3]['value'] === '' ? 'PlayerSubmissionFormt__input--invalid' : 'PlayerSubmissionFormt__input--valid' }
+            onChange={onInputChange} />
+          <input
+            name='verb'
+            placeholder={ formFields[4]['placeholder'] }
+            type="text"
+            value={ formFields[4]['value'] }
+            className={ formFields[4]['value'] === '' ? 'PlayerSubmissionFormt__input--invalid' : 'PlayerSubmissionFormt__input--valid' }
+            onChange={onInputChange} />
+          the
+          <input
+            name='adj2'
+            placeholder={ formFields[6]['placeholder'] }
+            type="text"
+            value={ formFields[6]['value'] }
+            className={ formFields[6]['value'] === '' ? 'PlayerSubmissionFormt__input--invalid' : 'PlayerSubmissionFormt__input--valid' }
+            onChange={onInputChange} />
+          <input
+            name='noun2'
+            placeholder={ formFields[7]['placeholder'] }
+            type="text"
+            value={ formFields[7]['value'] }
+            className={ formFields[7]['value'] === '' ? 'PlayerSubmissionFormt__input--invalid' : 'PlayerSubmissionFormt__input--valid' }
+            onChange={onInputChange} />
+          .
         </div>
 
         <div className="PlayerSubmissionForm__submit">
