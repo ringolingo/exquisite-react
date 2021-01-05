7fd9fbf9c657c154ca1ccac3f3b92525ad50e8e4
Ringo Alcock <ringo.alcock@gmail.com>
2021-01-05 15:03:39 -0800

    add onSubmitPoem function and conditional display

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -2,23 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './FinalPoem.css';
 
-const FinalPoem = ({submissions}) => {
+const FinalPoem = ({submissions, isSubmitted, revealPoem}) => {
   const formattedLines = submissions.map((line) => {
     return <p>{ line }</p>
   });
 
-  return (
-    <div className="FinalPoem">
-      <section className="FinalPoem__poem">
-        <h3>Final Poem</h3>
-          { formattedLines }
-      </section>
+  const onSubmitPoem = (event) => {
+    event.preventDefault();
 
-      <div className="FinalPoem__reveal-btn-container">
-        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
+    revealPoem();
+  };
+
+
+  if (isSubmitted) {
+    return (
+      <div className="FinalPoem">
+        <section className="FinalPoem__poem">
+          <h3>Final Poem</h3>
+            { formattedLines }
+        </section>
       </div>
-    </div>
-  );
+    )
+  } else {
+    return (
+      <div className="FinalPoem">
+        <div className="FinalPoem__reveal-btn-container">
+          <input type="button" value="We are finished: Reveal the Poem" onClick={onSubmitPoem} className="FinalPoem__reveal-btn" />
+        </div>
+      </div>
+    );
+  }
 }
 
 FinalPoem.propTypes = {
