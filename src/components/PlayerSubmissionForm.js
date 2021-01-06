c38e08eb94886a7cd6c2fab6cdf92c4692205aab
Ringo Alcock <ringo.alcock@gmail.com>
2021-01-05 16:17:42 -0800

    add createInputFields function to allow dynamically generated form

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import PlayerSubmissionFormField from './PlayerSubmissionFormField';
 
 import './PlayerSubmissionForm.css';
 
@@ -16,38 +15,6 @@ const PlayerSubmissionForm = ({sendSubmission, index, fields}) => {
       return field;
     }
   }));
-
-  // const createInputFields = (fields) => {
-  //   return fields.map((field) => {
-  //     if (field.key) {
-  //       return <input
-  //           name={field.key}
-  //           placeholder={ field.placeholder }
-  //           type="text"
-  //           value={ field.value }
-  //           className={ field.value === '' ? 'PlayerSubmissionFormt__input--invalid' : 'PlayerSubmissionFormt__input--valid' }
-  //           onChange={onInputChange} />
-  //     } else {
-  //       return field
-  //     }
-  //   })
-  // }
-
-  // const createInputFields = fields.map((field) => {
-  //     if (field.key) {
-  //       return <input
-  //           name={field.key}
-  //           placeholder={ field.placeholder }
-  //           type="text"
-  //           value={ field.value }
-  //           className={ field.value === '' ? 'PlayerSubmissionFormt__input--invalid' : 'PlayerSubmissionFormt__input--valid' }
-  //           onChange={onInputChange} />
-  //     } else {
-  //       return field
-  //     }
-  //   }
-  // );
-  
 
   const onInputChange = (event) => {
     const updatedFormFields = formFields.map((field) => {
@@ -90,6 +57,22 @@ const PlayerSubmissionForm = ({sendSubmission, index, fields}) => {
     }));
   };
 
+  const createInputFields = formFields.map((field) => {
+      if (field.key) {
+        return <input
+            name={field.key}
+            placeholder={ field.placeholder }
+            type="text"
+            value={ field.value }
+            className={ field.value === '' ? 'PlayerSubmissionFormt__input--invalid' : 'PlayerSubmissionFormt__input--valid' }
+            onChange={onInputChange}
+            />
+      } else {
+        return field
+      }
+    }
+  );
+
   return (
     <div className="PlayerSubmissionForm">
       <h3>Player Submission Form for Player #{ index }</h3>
@@ -97,53 +80,7 @@ const PlayerSubmissionForm = ({sendSubmission, index, fields}) => {
       <form className="PlayerSubmissionForm__form" onSubmit={ submitLine } >
 
         <div className="PlayerSubmissionForm__poem-inputs">
-          {/* { createInputFields } */}
-          {/* update these so they aren't just hardcoded to the formFields array indices */}
-          The 
-          <input
-            name='adj1'
-            placeholder={ formFields[1]['placeholder'] }
-            type="text"
-            value={ formFields[1]['value'] }
-            className={ formFields[1]['value'] === '' ? 'PlayerSubmissionFormt__input--invalid' : 'PlayerSubmissionFormt__input--valid' }
-            onChange={onInputChange} />
-          <input
-            name='noun1'
-            placeholder={ formFields[2]['placeholder'] }
-            type="text"
-            value={ formFields[2]['value'] }
-            className={ formFields[2]['value'] === '' ? 'PlayerSubmissionFormt__input--invalid' : 'PlayerSubmissionFormt__input--valid' }
-            onChange={onInputChange} />
-          <input
-            name='adv'
-            placeholder={ formFields[3]['placeholder'] }
-            type="text"
-            value={ formFields[3]['value'] }
-            className={ formFields[3]['value'] === '' ? 'PlayerSubmissionFormt__input--invalid' : 'PlayerSubmissionFormt__input--valid' }
-            onChange={onInputChange} />
-          <input
-            name='verb'
-            placeholder={ formFields[4]['placeholder'] }
-            type="text"
-            value={ formFields[4]['value'] }
-            className={ formFields[4]['value'] === '' ? 'PlayerSubmissionFormt__input--invalid' : 'PlayerSubmissionFormt__input--valid' }
-            onChange={onInputChange} />
-          the
-          <input
-            name='adj2'
-            placeholder={ formFields[6]['placeholder'] }
-            type="text"
-            value={ formFields[6]['value'] }
-            className={ formFields[6]['value'] === '' ? 'PlayerSubmissionFormt__input--invalid' : 'PlayerSubmissionFormt__input--valid' }
-            onChange={onInputChange} />
-          <input
-            name='noun2'
-            placeholder={ formFields[7]['placeholder'] }
-            type="text"
-            value={ formFields[7]['value'] }
-            className={ formFields[7]['value'] === '' ? 'PlayerSubmissionFormt__input--invalid' : 'PlayerSubmissionFormt__input--valid' }
-            onChange={onInputChange} />
-          .
+          { createInputFields }
         </div>
 
         <div className="PlayerSubmissionForm__submit">
